a09dc789a967baee7226ff5d4d315d4c4eaefbce
Mo <mohit@muthanna.com>
2023-05-14 17:13:28 -0400

    Initial implementation of fake server.

--- tests/router_test.rs
+++ tests/router_test.rs
@@ -2,17 +2,18 @@ use std::{sync::Arc, time::Duration};
 
 use anyhow::{bail, Result};
 use async_trait::async_trait;
-use log::{debug, info};
+use log::{debug, info, warn};
 use mobot::{
     api::{GetUpdatesRequest, Message, Update},
     *,
 };
 use tokio::sync::Mutex;
 
+#[derive(Debug, Clone)]
 struct TestMessage {
     chat_id: i64,
     text: String,
-    username: String,
+    from: String,
 }
 
 impl From<TestMessage> for Message {
@@ -20,13 +21,13 @@ impl From<TestMessage> for Message {
         Message {
             from: Some(api::User {
                 id: 1,
-                first_name: m.username.clone(),
-                username: Some(m.username.clone()),
+                first_name: m.from.clone(),
+                username: Some(m.from.clone()),
                 ..Default::default()
             }),
             chat: api::Chat {
                 id: m.chat_id,
-                username: Some(m.username),
+                username: Some(m.from),
                 ..Default::default()
             },
             text: Some(m.text),
@@ -35,12 +36,55 @@ impl From<TestMessage> for Message {
     }
 }
 
-#[derive(Default, Clone)]
+struct FakeChat {
+    chat_id: i64,
+    from: String,
+    chat_tx: Arc<tokio::sync::mpsc::Sender<TestMessage>>,
+}
+
+impl FakeChat {
+    pub async fn send_message(&self, text: impl Into<String>) -> Result<()> {
+        let text = text.into();
+        let chat_id = self.chat_id;
+        let from = self.from.clone();
+        let chat_tx = Arc::clone(&self.chat_tx);
+
+        Ok(chat_tx
+            .send(TestMessage {
+                chat_id,
+                text,
+                from,
+            })
+            .await?)
+    }
+}
+
 struct PostHandler {
     update_id: Arc<Mutex<i64>>,
+    chat_tx: Arc<tokio::sync::mpsc::Sender<TestMessage>>,
+    chat_rx: Arc<Mutex<tokio::sync::mpsc::Receiver<TestMessage>>>,
 }
 
 impl PostHandler {
+    pub fn new() -> Self {
+        let (tx, rx) = tokio::sync::mpsc::channel(100);
+
+        Self {
+            update_id: Arc::new(Mutex::new(0)),
+            chat_tx: Arc::new(tx),
+            chat_rx: Arc::new(Mutex::new(rx)),
+        }
+    }
+
+    pub fn create_chat(&self, from: impl Into<String>) -> FakeChat {
+        FakeChat {
+            // Generate random chat id
+            chat_id: rand::random(),
+            chat_tx: Arc::clone(&self.chat_tx),
+            from: from.into(),
+        }
+    }
+
     async fn get_updates(&self, req: GetUpdatesRequest) -> ApiResponse<Vec<Update>> {
         let update_id = {
             let mut update_id = self.update_id.lock().await;
@@ -48,23 +92,20 @@ impl PostHandler {
             *update_id
         };
 
-        if update_id > 3 {
-            tokio::time::sleep(Duration::from_secs(req.timeout.unwrap_or(1000) as u64)).await;
-            return ApiResponse::Ok(vec![]);
-        }
+        let mut rx = self.chat_rx.lock().await;
 
-        ApiResponse::Ok(vec![Update {
-            update_id,
-            message: Some(
-                TestMessage {
-                    chat_id: 1,
-                    text: "ping".to_string(),
-                    username: "foobar".to_string(),
-                }
-                .into(),
-            ),
-            ..Default::default()
-        }])
+        tokio::select! {
+            Some(msg) = rx.recv() => {
+                ApiResponse::Ok(vec![Update {
+                    update_id,
+                    message: Some(msg.into()),
+                    ..Default::default()
+                }])
+            }
+            _ = tokio::time::sleep(Duration::from_secs(req.timeout.unwrap_or(1000) as u64)) => {
+                ApiResponse::Ok(vec![])
+            }
+        }
     }
 }
 
@@ -74,7 +115,10 @@ impl Post for PostHandler {
         debug!("method = {}, req = {}", method, req);
         let response = match method.as_str() {
             "getUpdates" => self.get_updates(serde_json::from_str(req.as_str())?).await,
-            _ => ApiResponse::Err("Unknown method"),
+            _ => {
+                warn!("Unknown method: {}", method);
+                ApiResponse::Err(format!("Unknown method: {}", method))
+            }
         };
 
         let body = serde_json::to_string(&response).unwrap();
@@ -112,7 +156,12 @@ async fn handle_chat_event(
 #[tokio::test]
 async fn it_works() {
     mobot::init_logger();
-    let client = Client::new("token".to_string().into()).with_post_handler(PostHandler::default());
+    let fakeserver = PostHandler::new();
+    let chat = fakeserver.create_chat("qubyte");
+    chat.send_message("ping1").await.unwrap();
+    chat.send_message("ping2").await.unwrap();
+
+    let client = Client::new("token".to_string().into()).with_post_handler(fakeserver);
 
     // Keep the timeout short for testing.
     let mut router = Router::new(client).with_poll_timeout_s(1);
