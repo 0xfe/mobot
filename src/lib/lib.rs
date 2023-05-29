936b79cc1d228b3d0b019168cff7972f4db0f8c6
Mo <mohit@muthanna.com>
2023-05-28 20:59:35 -0400

    Added examples of helper methods.

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -197,6 +197,26 @@ async fn handle_chat_event(e: chat::Event, state: chat::State<()>) -> Result<cha
     Ok(chat::Action::Done)
 }
 ```
+
+Many API calls have helper methods on the [`chat::Event`] struct, so you can write the above
+example as:
+
+```no_run
+# use mobot::*;
+#
+async fn handle_chat_event(e: chat::Event, state: chat::State<()>) -> Result<chat::Action, anyhow::Error> {
+    match e.message.clone() {
+        chat::MessageEvent::New(message) => {
+            e.send_text(format!("Message: {}", message.text.unwrap())).await?;
+        }
+        chat::MessageEvent::Post(message) => {
+            e.send_markdown(format!("Channel post: {}", message.text.unwrap())).await?;
+        }
+        _ => anyhow::bail!("Unhandled update"),
+    }
+
+    Ok(chat::Action::Done)
+}
  */
 
 #[macro_use]
